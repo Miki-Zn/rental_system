e906052ba7db261db7d67844862da57c650c01b1
Michael <mznamenshchykov@gmail.com>
2025-05-10 00:48:43 +0200

    Add serializers, views, and API routing for listings, bookings, reviews, users

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Listing
+from .serializers import ListingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- rental_system/urls.py
+++ rental_system/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for rental_system project.
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-from django.urls import path
+from listings.views import ListingViewSet
+from bookings.views import BookingViewSet
+from reviews.views import ReviewViewSet
+from users.views import UserListView
+
+router = routers.DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
+router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/users/', UserListView.as_view(), name='user-list'),
 ]
+
